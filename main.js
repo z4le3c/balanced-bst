653f0fb552f29b4d48c8f13f55d8ec3e8076f6b7
zalec <142818531+z4le3c@users.noreply.github>
2023-12-08 21:27:08 -0600

    add prettyPrint

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const newTree = (arr) => {
       let mid = Math.floor((left + right) / 2)
 
       let root = newNode(arr[mid])
-      console.log(root.data)
       root.left = buildTree(left, mid - 1)
       root.right = buildTree(mid + 1, right)
 
@@ -35,7 +34,6 @@ const newTree = (arr) => {
 
     let mid = Math.floor(arr.length / 2)
     let root = newNode(arr[mid])
-    console.log(root.data)
     root.left = buildTree(0, mid-1)
     root.right = buildTree(mid+1, arr.length - 1)
 
@@ -48,4 +46,17 @@ const newTree = (arr) => {
   return tree
 }
 
-newTree([2,5,6,7,2,4,8,3])
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(newTree([2,5,6,7,2,4,8,3]).root)
